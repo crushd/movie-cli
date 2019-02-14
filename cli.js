2062fb0921b5e77d878aaa55c91227995e0c22da
codyrush <codyrush@gmail.com>
2019-02-13 20:15:08 -0800

    final updates

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 
-var fs = require("fs");
+
 var TV = require("./TV");
 
 var tvShow = new TV();
@@ -11,17 +11,6 @@ var search = [];
 
 for (i = 3; i < a.length; i++) {
     search.push(process.argv[i]);
-}
-
-function logResults() {
-
-    fs.appendFile("log.txt", text, function(err) {
-        // If an error was experienced we will log it.
-        if (err) {
-            console.log(err);
-        }
-    });
-
 }
 
 switch (action) {
